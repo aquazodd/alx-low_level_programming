65a34bbf288bedb976931e2ac3d716cd3c56ec4f
aquazodd <yohannesabiti309412@gmail.com>
2023-03-20 10:49:55 -0700

    commit hello world

---
+++ 0x00-hello_world/6-print_numberz.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main
+ *
+ * Return: 0 after successful execution
+ */
+int main(void)
+{
+int a;
+for (a = 0; a <= 10; a++)
+{
+printf(a);
+}
+printf("\n");
+return (0);
+}
