e66bed7d324ea406b502956606b1dc7b935c2365
aquazodd <yohannesabiti309412@gmail.com>
2023-03-21 10:52:01 -0700

    added a working folder and made a c file that prints putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * main - Entry point
+ *
+ * return 0 after code succesion
+ */
+int _putchar(char c)
+{
+	int n;
+	char c = "putchar";
+	for (n=0;n < 1; n++)
+	{
+		_putchar(c + '\n');
+	}
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+a folder to experiment on functions and nested loops
