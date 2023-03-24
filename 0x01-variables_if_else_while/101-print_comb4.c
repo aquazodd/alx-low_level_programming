c66148c2b030dd283043bc581ccc663d99d5ad5a
aquazodd <yohannesabiti309412@gmail.com>
2023-03-24 08:01:51 -0700

    added new editted file

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -8,29 +8,29 @@ int main(void)
 {
         int c;
         int d;
-		int e;
-		
-        for (c = 48; c <= 57; c++)
+	int e;
+
+	for (c = 48; c <= 57; c++)
         {
                 for (d = 48; d <= 57; d++)
-                {
-                	for (e = 48; e <= 57; e++)
-                	{
-                		if (c != d && c != e && d != e && c < d && d < e)
-                		{
-							    putchar(c);
-                                putchar(d);
-                                putchar(e);
-                                if (c + d + e != 168)
-                                {
-                                	putchar(',');
-                                	putchar(' ');	
-								}
-                    	}
-                    }
-                }
-        }
-        putchar('\n');
-        return (0);
+		{
+			for (e = 48; e <= 57; e++)
+			{
+				if (c != d && c != e && d != e && c < d && d < e)
+				{
+					putchar(c);
+					putchar(d);
+					putchar(e);
+					if (c + d + e != 168)
+					{
+						putchar(',');
+						putchar(' ');
+					}
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
 }
 
