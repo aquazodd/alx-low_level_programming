ee5e682549313b5d151e4781b7a3de6284e3dad6
aquazodd <yohannesabiti309412@gmail.com>
2023-03-24 07:55:27 -0700

    added new editted file

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: 0 after successful execution
+ */
+int main(void)
+{
+        int c;
+        int d;
+		int e;
+		
+        for (c = 48; c <= 57; c++)
+        {
+                for (d = 48; d <= 57; d++)
+                {
+                	for (e = 48; e <= 57; e++)
+                	{
+                		if (c != d && c != e && d != e && c < d && d < e)
+                		{
+							    putchar(c);
+                                putchar(d);
+                                putchar(e);
+                                if (c + d + e != 168)
+                                {
+                                	putchar(',');
+                                	putchar(' ');	
+								}
+                    	}
+                    }
+                }
+        }
+        putchar('\n');
+        return (0);
+}
+
