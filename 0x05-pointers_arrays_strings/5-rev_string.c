311032d33bf3ad71c1b6e8c3f7d00c8bcee2632c
aquazodd <yohannesabiti309412@gmail.com>
2023-03-27 12:41:16 -0700

    made more edits

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -1,11 +1,11 @@
+#include<stdio.h>
 #include "main.h"
-#include <stdio.h>
 /**
  * rev_string - prints reverse of a string
  * @s: string
  */
 void rev_string(char *s)
 {
-	printf("%s", strrev(s));
+	printf("%c", strrev(s));
 	return 0;
 }
