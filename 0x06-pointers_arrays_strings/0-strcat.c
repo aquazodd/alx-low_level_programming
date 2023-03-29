f5f0df936ddbaaa6a93805681b92b8dc50e0c90e
aquazodd <yohannesabiti309412@gmail.com>
2023-03-29 06:55:38 -0700

    added code to file

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,10 +1,11 @@
 #include "main.h"
-/*
- *
- *
- *
+/**
+ * _strcat - concatnates two strings
+ * 0dest: a pointer
+ * @src: a pointer
+ * Return: dest
  */
 char *_strcat(char *dest, char *src)
 {
-	strcat(*dest, *src);
+	strcat(dest, src);
 }
